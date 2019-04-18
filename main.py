43b526cfb9a6959bf30cd18a3c9dac2d6108b702
Stanislav Nezamov <shaxraman@gmail.com>
2019-04-18 23:05:42 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# v1.0
+# Выдае url для скачивания файла
+
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+    }
+data = {'os_username': 'login',
+        'os_password': 'password',
+        'login': 'Войти',
+        'os_destination': '/index.action',
+        }
+url = 'https://wiki.i-core.ru/pages/viewpage.action?pageId=10551321'
+
+s = requests.Session()
+s.post(url, headers=headers, data=data)
+a = s.get(url).text
+# print(a)
+
+
+soup = BeautifulSoup(a, 'html.parser')
+link = soup.find('a', id='action-export-pdf-link', href=True).get('href')
+
+print('new_url -', 'https://wiki.i-core.ru' + link)
