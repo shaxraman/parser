d514dd4eda90b83e5e5debbcdd8f5692b4b44e46
Stanislav Nezamov <shaxraman@gmail.com>
2019-04-20 22:18:54 +0300

    get better
    Оптимизировал код

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# v1.1
+# v1.2
 # Выдае url для скачивания файла
 # Не забыть ввести логин и пароль
 
 import requests
 from bs4 import BeautifulSoup
-from log import login, password                 # Хранятся логины и пароли
+from log import login, password                         # Хранятся логины и пароли
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
@@ -17,24 +17,17 @@ data = {'os_username': login,      # Логин
 url = 'https://wiki.i-core.ru/pages/viewpage.action?pageId=10551321'
 
 
-def loggin_to_site(url):
-    s = requests.Session()                      # Для сохранения сессии
-    s.post(url, headers=headers, data=data)     # Лонинемся на сайт
-    #print(a.headers)
-    return s.get(url).text                      # Возвращает html страницу
-
+def get_html(url, session):
+    return session.get(url).text                    # Возвращает html страницу
 
 def get_pdf_link(html):
     soup = BeautifulSoup(html, 'html.parser')
     link = soup.find('a', id='action-export-pdf-link', href=True).get('href')
-    # print('url for download pdf -', 'https://wiki.i-core.ru' + link)
-    return 'https://wiki.i-core.ru' + link      # Возвращает url для скачивания PDF файла
+    #  print('url for download pdf -', 'https://wiki.i-core.ru' + link)
+    return 'https://wiki.i-core.ru' + link              # Возвращает url для скачивания PDF файла
 
-def download_file(url):
-    s = requests.Session()                      # Для сохранения сессии
-    s.post(url, headers=headers, data=data)
-    r = s.get(url, stream=True)
-
+def download_file(url, session):
+    r = session.get(url, stream=True)
     print(r.headers)
     print(r.url)
 
@@ -42,8 +35,13 @@ def download_file(url):
         for chunk in r.iter_content(8192):
             f.write(chunk)
 
-if __name__ == '__main__':
-    logging = loggin_to_site(url)
-    link = get_pdf_link(logging)
+def main():
+    with requests.Session() as s:                                           # Для сохранения сессии
+        s.post('https://wiki.i-core.ru', headers=headers, data=data)        # Логинемся на сайт
+        link = get_pdf_link(get_html(url, s))
+        download_file(link, s)
 
-    download_file(link)
+
+
+if __name__ == '__main__':
+    main()
